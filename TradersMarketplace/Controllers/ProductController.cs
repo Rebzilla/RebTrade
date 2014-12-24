b5b39aadd7a5ceac9a97ee33e184248532f1142f
Rebzilla <reb.mintoff@gmail.com>
2014-12-24 18:31:16 +0100

    Implemented Add Product functionality for Seller

---
+++ TradersMarketplace/Controllers/ProductController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer;
+using Common;
+using TradersMarketplace.Models;
+
+namespace TradersMarketplace.Controllers
+{
+    public class ProductController : MenusController
+    {
+        //
+        // GET: /Product/
+
+        public ActionResult Index()
+        {
+            ProductModel pm = new ProductModel();
+            return View(pm);
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(ProductModel data, HttpPostedFileBase file)
+        {
+            if(ModelState.IsValid)
+            {
+                if (file != null && file.ContentLength > 0)
+                {
+                    string absolutePathOfImagesFolder = Server.MapPath("\\Images");
+                    string filename = Guid.NewGuid().ToString() + System.IO.Path.GetExtension(file.FileName);
+                    file.SaveAs(absolutePathOfImagesFolder + "\\" + filename);
+
+                    ProductsBL pbl = new ProductsBL();
+                    pbl.AddProduct(data.ProductName, HttpContext.User.Identity.Name, data.Description, data.Price, "\\Images\\" + filename, data.StockQty, data.CategoryID);
+                    return RedirectToAction("Index", "Product"); //redirect to view products page
+                }
+            }
+            return View();
+        }
+
+        public ActionResult EditProduct(ProductModel data)
+        {
+            if(ModelState.IsValid)
+            {
+                ProductsBL pbl = new ProductsBL();
+                int catID = pbl.GetCategory(data.CategoryName).CategoryID;
+
+                ViewBag.Message = "Product has been modified successfully";
+            }
+            return View();
+        }
+
+    }
+}
---
+++ TradersMarketplace/Models/ProductModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using BusinessLayer;
+using Common;
+using System.Web.Mvc;
+
+namespace TradersMarketplace.Models
+{
+    public class ProductModel
+    {
+        [Required]
+        public string ProductName { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:n2}", ApplyFormatInEditMode = true)]
+        public decimal Price { get; set; }
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int StockQty { get; set; }
+        public string CategoryName { get; set; }
+        public int CategoryID { get; set; }
+        public SelectList Categories { get; set; }
+        IList<ProductCategory> categories { get; set; }
+
+        public ProductModel()
+        {
+            categories = new ProductsBL().GetProductCategories().ToList();
+            Categories = new SelectList(categories, "CategoryID", "CategoryName");
+        }
+    }
+}
