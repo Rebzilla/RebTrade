4711985872709e08486eb599efe9a511c58b6535
Rebzilla <reb.mintoff@gmail.com>
2014-12-26 23:11:07 +0100

    Added the Edit Functionality for Seller

--- TradersMarketplace/Controllers/ViewSellerProductController.cs
+++ TradersMarketplace/Controllers/ViewSellerProductController.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TradersMarketplace.Models;
 using Common;
 using Common.Views;
 using BusinessLayer;
+using System.Net;
+using System.Data;
+
 
 namespace TradersMarketplace.Controllers
 {
@@ -13,6 +17,7 @@ namespace TradersMarketplace.Controllers
     {
         //
         // GET: /ViewSellerProduct/
+        public TradersMarketPlaceEntities db = new TradersMarketPlaceEntities();
 
         [Authorize]
         public ActionResult Index()
@@ -27,7 +32,58 @@ namespace TradersMarketplace.Controllers
             new ProductsBL().DeleteProduct(pID);
         }
 
-        //edit by product id, partial view
+        //get by product id
+        public ActionResult EditProduct(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            List<SelectListItem> categories = new List<SelectListItem>();
+            IList<ProductCategory> category = new ProductsBL().GetProductCategories().ToList();
+            foreach (ProductCategory pc in category)
+            {
+                categories.Add(new SelectListItem { Text = pc.CategoryName, Value = pc.CategoryID.ToString() });
+            }
+            ViewData["categoriesList"] = categories;
+            return View(product);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditProduct([Bind(Include = "ProductID,SellerUsername,ProductName,Description,Price,StockQty,CategoryID")] Product product, HttpPostedFileBase file)
+        {
+            string prevImageLink = new ProductsBL().GetProductByID(product.ProductID).ImageLink;
+            string filename = "";
+            if (file != null && file.ContentLength > 0)
+            {
+                string absolutePathOfImagesFolder = Server.MapPath("\\Images");
+                filename = Guid.NewGuid().ToString() + System.IO.Path.GetExtension(file.FileName);
+                file.SaveAs(absolutePathOfImagesFolder + "\\" + filename);
+                product.ImageLink = ("\\Images\\" + filename).ToString();
+            }
+            else
+            {
+                product.ImageLink = prevImageLink;
+            }
+            product.SellerUsername = User.Identity.Name;
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryID = new SelectList(db.ProductCategories, "CategoryID", "Name", product.CategoryID);
+            ViewBag.Username = new SelectList(db.Users, "Username", "Password", product.SellerUsername);
+            return View(product);
+        }
+
 
     }
 }
