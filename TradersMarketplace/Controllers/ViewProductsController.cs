de8131fb27d5e6efd7a322bca729888be6743505
Rebzilla <reb.mintoff@gmail.com>
2014-12-28 13:17:12 +0100

    Implemented the View Products function for Buyers

---
+++ TradersMarketplace/Controllers/ViewProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Common;
+using BusinessLayer;
+using Common.Views;
+
+namespace TradersMarketplace.Controllers
+{
+    public class ViewProductsController : MenusController
+    {
+        //
+        // GET: /ViewProducts/
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            List<ProductsView> products = new ProductsBL().GetAllProducts().ToList();
+            return View(products);
+        }
+
+    }
+}
