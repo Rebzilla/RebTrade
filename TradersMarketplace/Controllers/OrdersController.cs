be8721c6618e50b6a542ad079af7dbabd7179a2a
Rebzilla <reb.mintoff@gmail.com>
2014-12-29 14:48:37 +0100

    Implemented the Edit Order Status functionality for the Seller

--- TradersMarketplace/Controllers/OrdersController.cs
+++ TradersMarketplace/Controllers/OrdersController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using BusinessLayer;
 using Common;
 using Common.Views;
+using System.Net;
 
 namespace TradersMarketplace.Controllers
 {
@@ -21,7 +22,7 @@ namespace TradersMarketplace.Controllers
             List<SelectListItem> orderStatuses = new List<SelectListItem>();
             IList<OrderStatu> os = new OrdersBL().GetOrderStatuses().ToList();
             foreach (OrderStatu o in os)
-            { 
+            {
                 orderStatuses.Add(new SelectListItem { Text = o.Status, Value = o.StatusID.ToString() });
             }
             ViewData["statusList"] = orderStatuses;
@@ -44,17 +45,41 @@ namespace TradersMarketplace.Controllers
             return View(orderDetails);
         }
 
-        public ActionResult EditOrder(Guid id)
+        //[HttpPost]
+        public ActionResult UpdateOrderStatus(Guid OrderID, int statusID) //pass the selected statusID
         {
-           // new OrdersBL().UpdateOrderStatusByOrderID(id, statusID, buyer, orderDate);
+            Order originalOrder = new OrdersBL().GetOrderByID(OrderID);
+            new OrdersBL().UpdateOrderStatusByOrderID(originalOrder.OrderID, statusID, originalOrder.Username, originalOrder.OrderDate);
+
+            //Populate the dropdown list
+            List<SelectListItem> orderStatuses = new List<SelectListItem>();
+            IList<OrderStatu> os = new OrdersBL().GetOrderStatuses().ToList();
+            foreach (OrderStatu o in os)
+            {
+                orderStatuses.Add(new SelectListItem { Text = o.Status, Value = o.StatusID.ToString() });
+            }
+            ViewData["statusList"] = orderStatuses;
+
+            //Get the orders to display
             List<OrdersView> orders = (List<OrdersView>)new OrdersBL().GetOrdersForSeller(HttpContext.User.Identity.Name).ToList();
+
             return View("Index", orders);
         }
 
         public ActionResult DeleteOrder(Guid id)
         {
-            Order o = new OrdersBL().GetOrderByID(id);
-            new OrdersBL().UpdateOrderStatusByOrderID(o.OrderID, 3, o.Username, o.OrderDate); //set status to complete //3
+            Order originalOrder = new OrdersBL().GetOrderByID(id);
+            new OrdersBL().UpdateOrderStatusByOrderID(originalOrder.OrderID, 3, originalOrder.Username, originalOrder.OrderDate); //set status to complete id = 3
+
+            //Populate the dropdown list
+            List<SelectListItem> orderStatuses = new List<SelectListItem>();
+            IList<OrderStatu> os = new OrdersBL().GetOrderStatuses().ToList();
+            foreach (OrderStatu o in os)
+            {
+                orderStatuses.Add(new SelectListItem { Text = o.Status, Value = o.StatusID.ToString() });
+            }
+            ViewData["statusList"] = orderStatuses;
+
             List<OrdersView> orders = (List<OrdersView>)new OrdersBL().GetOrdersForSeller(HttpContext.User.Identity.Name).ToList();
             return View("Index", orders);
         }
